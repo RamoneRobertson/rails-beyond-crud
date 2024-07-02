128738fb73a26c37b2ff73b625715193bbd0475d
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-02 09:28:38 +0900

    Checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
+    resources :reviews, only:[:new, :create]
     # Additional routes that relate to restaurants go here:
     collection do
       #  " custom index "
@@ -13,6 +14,10 @@ Rails.application.routes.draw do
       get :chef
     end
   end
+
+  resources :reviews, only:[:destroy]
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
